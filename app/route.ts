82bc26191af3c3b0fda9d6ad90303aa46b376989
Akshath Pandey <akshath.centaurus@gmail.com>
2024-01-21 13:40:40 +0530

    Routes

--- app/route.ts
+++ app/route.ts
@@ -1,5 +1,36 @@
-import NextAuth from "next-auth"
-import { authOptions } from "@/auth";
-const handler = NextAuth( authOptions );
+import { adminDb } from "@/firebase-admin";
+import { NextResponse } from "next/server";
 
-export { handler as GET, handler as POST }
+export async function DELETE(req: Request){
+    const {chatId} = await req.json();
+    const ref = adminDb.collection("chats").doc(chatId);
+    const bulkWriter = adminDb.bulkWriter();
+    const MAX_RETRY_ATTEMPTS =5;
+
+    bulkWriter.onWriteError((error) =>{
+        if(error.failedAttempts< MAX_RETRY_ATTEMPTS){
+            return true;
+        } else{
+            console.log("Failed write at document",error.documentRef.path);
+            return false;                   
+        }
+    });
+
+    try {
+        await adminDb.recursiveDelete(ref,bulkWriter);
+        return NextResponse.json(
+            {
+                success: true,
+            },
+            { status:200}
+        );
+    } catch (error) {
+        console.error("Promise rejected: ", error);
+        return NextResponse.json(
+            {
+                success: false,
+            },
+            { status: 500 }
+        );
+    }
+}
